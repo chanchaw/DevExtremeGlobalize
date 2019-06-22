a9bffa9d262d8658d5979755e75b8093443929ea
chanchaw <409223171@qq.com>
2019-06-22 11:06:48 +0800

    上传项目

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,68 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { SideNavOuterToolbarModule, SideNavInnerToolbarModule, SingleCardModule } from './layouts';
+import { FooterModule, LoginFormModule } from './shared/components';
+import { AuthService, ScreenService, AppInfoService } from './shared/services';
+import { AppRoutingModule } from './app-routing.module';
+
+
+
+// 下面开始是国际化代码
+import "devextreme/localization/globalize/number";
+import "devextreme/localization/globalize/date";
+import "devextreme/localization/globalize/currency";
+import "devextreme/localization/globalize/message";
+ 
+// Dictionaries for German and Russian languages
+// import deMessages from "devextreme/localization/messages/de.json!json";
+// import ruMessages from "devextreme/localization/messages/ru.json!json";
+ 
+// Common and language-specific CLDR JSONs
+// 在 2019年6月22日 09:38:24 测试这里代码要注释掉，使用下面的方式导入对应语言的json
+// import supplemental from "devextreme-cldr-data/supplemental.json!json";
+// import deCldrData from "devextreme-cldr-data/de.json!json";
+// import ruCldrData from "devextreme-cldr-data/ru.json!json";
+
+ 
+// In projects created with Angular CLI 6+
+import deMessages from "devextreme/localization/messages/de.json";
+import ruMessages from "devextreme/localization/messages/ru.json";
+import zhMessages from "devextreme/localization/messages/zh.json";
+import supplemental from "devextreme-cldr-data/supplemental.json";
+import deCldrData from "devextreme-cldr-data/de.json";
+import ruCldrData from "devextreme-cldr-data/ru.json";
+import zhCldrData from "devextreme-cldr-data/zh.json";
+ 
+import Globalize from "globalize";
+ 
+Globalize.load(
+    supplemental, deCldrData, ruCldrData,zhCldrData
+);
+ 
+Globalize.loadMessages(deMessages);
+Globalize.loadMessages(ruMessages);
+Globalize.loadMessages(zhMessages);
+ 
+Globalize.locale(navigator.language);
+// 国际化代码结束
+
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    SideNavOuterToolbarModule,
+    SideNavInnerToolbarModule,
+    SingleCardModule,
+    FooterModule,
+    LoginFormModule,
+    AppRoutingModule
   ],
-  providers: [],
+  providers: [AuthService, ScreenService, AppInfoService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
